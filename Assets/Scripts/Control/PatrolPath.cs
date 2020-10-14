461d2a5a1de9cdf49093501a70442d19f09f941f
Michael Numrich <pc@pop-os.localdomain>
2020-10-13 21:53:26 -0600

    Working on: waypoint gizmos

---
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{	
+	public class PatrolPath : MonoBehaviour 
+	{
+		private void OnDrawGizmos() 
+		{
+			for (int i = 0; i < transform.childCount; i++)
+			{
+				// transform.GetChild(i);;
+				Gizmos.color = Color.green;
+				Gizmos.DrawSphere(transform.GetChild(i), 1f);
+			}
+		}
+	}
+}
