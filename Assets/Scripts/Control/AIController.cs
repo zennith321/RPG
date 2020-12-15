da33234ae45ed488914d5d4eb4329bf8bac85ed9
Michael Numrich <mnumrich@hotmail.com>
2020-12-14 17:14:02 -0700

    Implemented patrol behaviour

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using RPG.Combat;
@@ -11,6 +12,8 @@ namespace RPG.Control
 	{
 		[SerializeField] float chaseDistance = 5f;
 		[SerializeField] float suspicionTime = 3f;
+		[SerializeField] PatrolPath patrolPath;
+		[SerializeField] float waypointTolerance = 1f;
 		
 		GameObject player;
 		ActionScheduler actionScheduler;
@@ -20,6 +23,7 @@ namespace RPG.Control
 
 		Vector3 guardPosition;
 		float timeSinceLastSawPlayer = Mathf.Infinity;
+		int currentWaypointIndex = 0;
 
 		private void Start() {
 			actionScheduler = GetComponent<ActionScheduler>();
@@ -46,15 +50,42 @@ namespace RPG.Control
 			}
 			else
 			{
-				GuardBehaviour();
+				PatrolBehaviour();
 			}
 
 			timeSinceLastSawPlayer += Time.deltaTime;
 		}
 
-		private void GuardBehaviour()
+		private void PatrolBehaviour()
 		{
-			mover.StartMoveAction(guardPosition);
+			Vector3 nextPosition = guardPosition;
+
+			if(patrolPath != null)
+			{
+				if(AtWaypoint())
+				{
+					CycleWaypoint();
+				}
+				nextPosition = GetCurrentWaypoint();
+			}
+
+			mover.StartMoveAction(nextPosition);
+		}
+
+		private bool AtWaypoint()
+		{
+			float distanceToWaypoint = Vector3.Distance(transform.position, GetCurrentWaypoint());
+			return distanceToWaypoint < waypointTolerance;
+		}
+
+		private void CycleWaypoint()
+		{
+			currentWaypointIndex = patrolPath.GetNextIndex(currentWaypointIndex);
+		}
+
+		private Vector3 GetCurrentWaypoint()
+		{
+			return patrolPath.GetWaypoint(currentWaypointIndex);
 		}
 
 		private void SuspicionBehaviour()
