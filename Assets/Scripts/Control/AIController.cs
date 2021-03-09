f33a66954e45c3b37c6794e76aab368e3e1f3e4d
Michael Numrich <mnumrich@hotmail.com>
2021-03-08 18:11:21 -0700

    Added a variance in speed for Enemies

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -15,6 +15,8 @@ namespace RPG.Control
 		[SerializeField] PatrolPath patrolPath;
 		[SerializeField] float waypointTolerance = 1f;
 		[SerializeField] float waypointDwellTime = 2f;
+		[Range(0,1)]
+		[SerializeField] float patrolSpeedFraction = 0.2f;
 		
 		GameObject player;
 		ActionScheduler actionScheduler;
@@ -78,7 +80,7 @@ namespace RPG.Control
 
 			if (timeSinceArrivedAtWaypoint > waypointDwellTime)
 			{
-				mover.StartMoveAction(nextPosition);
+				mover.StartMoveAction(nextPosition, patrolSpeedFraction);
 			}
 		}
 
