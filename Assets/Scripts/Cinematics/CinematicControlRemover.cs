3bc43d7a1d9e2647dd9d21b17c9441a6e1dd8550
Michael Numrich <mnumrich@hotmail.com>
2021-03-09 19:24:54 -0700

    Fixed bugs with intro cinematic

---
+++ Assets/Scripts/Cinematics/CinematicControlRemover.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.Playables;
+using RPG.Core;
+using RPG.Control;
+
+namespace RPG.Cinematics
+{
+	public class CinematicControlRemover : MonoBehaviour
+	{
+        GameObject player = null;
+        PlayerController playerController = null;
+        ActionScheduler actionScheduler = null;
+
+        private void Start() 
+        {
+			player = GameObject.FindWithTag("Player");
+			playerController = player.GetComponent<PlayerController>();
+            actionScheduler = player.GetComponent<ActionScheduler>();
+
+            GetComponent<PlayableDirector>().played += DisableControl;
+			GetComponent<PlayableDirector>().stopped += EnableControl;
+        }	
+
+        void DisableControl(PlayableDirector playableDirector)
+        {
+			actionScheduler.CancelCurrentAction();
+			playerController.enabled = false;
+        }
+
+        void EnableControl(PlayableDirector playableDirector)
+        {
+			playerController.enabled = true;
+		}
+    }
+}
---
+++ Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+        bool isTriggered = false;
+
+        private void OnTriggerEnter(Collider other) 
+        {
+            if (isTriggered) return;
+            if (other.tag == "Player")
+            {
+                StartSequence();
+            }
+		}
+
+		private void StartSequence()
+		{
+			GetComponent<PlayableDirector>().Play();
+			isTriggered = true;
+		}
+	}
+}
