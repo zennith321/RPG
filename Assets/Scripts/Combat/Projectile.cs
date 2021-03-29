4340ccaffa711574f3d4cf5d2ba9b6a0ee77a6eb
Michael Numrich <mnumrich@hotmail.com>
2021-03-28 20:28:45 -0600

    Added flair to arrow projectiles

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -7,8 +7,8 @@ namespace RPG.Combat
 	public class Projectile : MonoBehaviour 
 	{
 		[SerializeField] float speed = 1f;
-		
 		Health target = null;
+		float damage = 0;
 
 		private void Update() 
 		{
@@ -18,9 +18,10 @@ namespace RPG.Combat
 			transform.Translate(Vector3.forward * speed * Time.deltaTime);
 		}
 
-		public void SetTarget(Health target)
+		public void SetTarget(Health target, float damage)
 		{
 			this.target = target;
+			this.damage = damage;
 		}
 
 		private Vector3 GetAimLocation()
@@ -31,6 +32,14 @@ namespace RPG.Combat
 				return target.transform.position;
 			}
 			return target.transform.position + Vector3.up * targetCapsule.height / 2; //Aim at the middle of the capsule collider
+		}
+
+		private void OnTriggerEnter(Collider other) 
+		{
+			//is collider other of type health target
+			if(other.GetComponent<Health>() != target) return;
+			target.TakeDamage(damage);
+			Destroy(this.gameObject);
 		}
 	}
 }
