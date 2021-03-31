002865b4ef144f199afd42abd001a7b4ae872f15
Michael Numrich <mnumrich@hotmail.com>
2021-03-31 16:58:14 -0600

    Implemented platform for character stats

---
+++ Assets/Scripts/Stats/BaseStats.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace RPG.Stats
+{
+	public class BaseStats : MonoBehaviour
+	{
+		[Range(1,99)]
+		[SerializeField] int startingLevel = 1;
+		[SerializeField] CharacterClass characterClass;
+	}
+}
