2c743b8e165d827e5ec0cb36ac7c5fa71a229651
Michael Numrich <mnumrich@hotmail.com>
2021-04-03 20:16:48 -0600

    Performance improvement in progression SO

--- Assets/Scripts/Stats/Progression.cs
+++ Assets/Scripts/Stats/Progression.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System;
 
 namespace RPG.Stats
 {
@@ -7,22 +9,39 @@ namespace RPG.Stats
 	{
 		[SerializeField] ProgressionCharacterClass[] characterClasses = null;
 
+		Dictionary<CharacterClass, Dictionary<Stat, float[]>> lookupTable = null;
+
 		public float GetStat(Stat stat, CharacterClass characterClass, int level)
 		{
-			foreach(ProgressionCharacterClass progressionClass in characterClasses)
+			BuildLookup();
+
+			float[] levels = lookupTable[characterClass][stat];
+
+			if (levels.Length < level)
 			{
-				if (progressionClass.characterClass != characterClass) continue;
-				
-				foreach (ProgressionStat progressionStat in progressionClass.stats) //TODO review all this nonsense to try to understand it, lecture 136
-				{
-					if (progressionStat.stat != stat) continue;
-
-					if(progressionStat.levels.Length < level)	continue;
-
-					return progressionStat.levels[level - 1];
-				}
+				return 0;
 			}
-			return 0;
+
+			return levels[level - 1];
+		}
+
+		private void BuildLookup()
+		{
+			if(lookupTable != null) return;
+
+			lookupTable = new Dictionary<CharacterClass, Dictionary<Stat, float[]>>();
+
+			foreach (ProgressionCharacterClass progressionClass in characterClasses)
+			{
+				var statLookupTable = new Dictionary<Stat, float[]>();
+
+				foreach (ProgressionStat progressionStat in progressionClass.stats)
+				{
+					statLookupTable[progressionStat.stat] = progressionStat.levels;
+				}
+
+				lookupTable[progressionClass.characterClass] = statLookupTable;
+			}
 		}
 
 		[System.Serializable]
